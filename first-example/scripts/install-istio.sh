0a5073de5e438ac75c46eab6f63c5803c712c78b
Florian Bacher <bacherfl@gmail.com>
2019-01-22 17:25:02 +0100

    restart production namespace pods after istio installation

--- first-example/scripts/install-istio.sh
+++ first-example/scripts/install-istio.sh
@@ -12,3 +12,7 @@ kubectl label namespace production istio-injection=enabled
 kubectl create -f ../manifests/istio/istio-gateway.yml
 
 ./create-service-entry.sh $DT_TENANT_ID $DT_PAAS_TOKEN
+
+sleep 10
+
+kubectl delete pods --all -n production
