27cd4f14631ed0f51be744103737f8a1b02a559c
TannerGabriel <gabrieltanner.code@gmail.com>
2021-12-16 19:03:27 +0100

    Fix echo formatting
    Signed-off-by: TannerGabriel <gabrieltanner.code@gmail.com>

--- quickstart/multistage-delivery.sh
+++ quickstart/multistage-delivery.sh
@@ -101,9 +101,10 @@ echo "git clone https://github.com/keptn/examples --single-branch"
 
 DIR="./examples"
 BOLD='\033[1m'
+NORMAL='\033[0m'
 if [ -d "$DIR" ]; then
   # Example directory already exists
-  echo -e "\n${BOLD}Example directory already exists in folder. Skipping download!"
+  echo -e "\n${BOLD}Example directory already exists in folder. Skipping download!${NORMAL}"
 else
   # Example Directory does not exist
   git clone https://github.com/keptn/examples --single-branch
